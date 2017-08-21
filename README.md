3f931f33581009100acfdc135de43bf4aacca761
jackhasakeyboard <jackhasakeyboard@gmail.com>
2017-08-21 02:47:42 -0600

    Mass commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Thanks to these ColourLovers users for the colors:
+
+Wolf Tyrant - http://www.colourlovers.com/palette/396059/white_wolf
+Sky - http://www.colourlovers.com/palette/1309732/Freshhh
--- app/controllers/color_controller.rb
+++ app/controllers/color_controller.rb
@@ -7,9 +7,9 @@ class ColorController < ApplicationController
 				tertiary: "#107c10"
 			},
 			"Sky": {
-				primary: "grey",
-				secondary: "blue",
-				tertiary: "white"
+				primary: "#22ADC2",
+				secondary: "#F3F3F3",
+				tertiary: "#22ADC2"
 			},
 			"Burgundy": {
 				primary: "white",
@@ -35,7 +35,62 @@ class ColorController < ApplicationController
 				primary: "white",
 				secondary: "#262626",
 				tertiary: "#00140E"
-			}
+			},
+			"Walk in the Sun": {
+				primary: "pink",
+				secondary: "orange",
+				tertiary: "white"
+			},
+			"Walk in the Sun 2": {
+				primary: "light blue",
+				secondary: "pink",
+				tertiary: "orange"
+			},
+			"Walk in the Sun 3": {
+				primary: "#B0BCBF",
+				secondary: "#A3ABAD",
+				tertiary: "#666666"
+			},
+			"Gas Station": {
+				primary: "#161616",
+				secondary: "#6BC49D",
+				tertiary: "#EFA5E8"
+			},
+			"Stormtrooper": {
+				primary: "#fff",
+				secondary: "#000",
+				tertiary: "grey"
+			},
+			"Raven": {
+				primary: "#00243A",
+				secondary: "#000",
+				tertiary: "#fff"
+			},
+			"Wolf Tyrant": {
+				primary: "#B0BCBF",
+				secondary: "#A3ABAD",
+				tertiary: "#666666"
+			},
+			"Urbnet": {
+				primary: "#161616",
+				secondary: "#222",
+				tertiary: "yellow"
+			},
+			"Freshly Squeezed": {
+				primary: "#F4974D",
+				secondary: "#222",
+				tertiary: "yellow"
+			},
+			"Bright Green": {
+				primary: "lime",
+				secondary: "#222",
+				tertiary: "yellow"
+			},
+			"Sweden": {
+				primary: "#161616",
+				secondary: "blue",
+				tertiary: "yellow"
+			},
 		}
 	end
 end
